89c356a36a3ad6b6d582f139e9838c75f64812fc
IainMcDUoM <iainmcdonald90@googlemail.com>
2015-11-10 06:49:30 -0800

    moar stuff

--- planets_parser.py
+++ planets_parser.py
@@ -1,11 +1,13 @@
 import xml.etree.ElementTree as tree
-
+print "Source"
 planets_tree = tree.parse('planets.xml')
 root=planets_tree.getroot()
 print root.attrib['name']
 
+print "list of planets"
 for child in root:
      print "tag=",child.tag, " attrib=", child.attrib['name']
 
-for element  in planets_tree.iter():
-    print element.tag, element.attrib
+print "list of moons"
+for element  in planets_tree.iter(tag='moon'):
+    print element.attrib
